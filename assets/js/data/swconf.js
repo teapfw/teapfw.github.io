b0f39153ed9d36ee5f86b4ac7904dd3420ddb3c3
William Freidank <37943506+teapfw@users.noreply.github.com>
2025-04-29 16:10:34 +0200

    new version Tue Apr 29 16:10:34 CEST 2025

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737946999',resources: [
+    cacheName: 'chirpy-1745935834',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
