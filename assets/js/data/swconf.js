9b22189dc8e5df1ab201f36ec8b83d59059497ad
William Freidank <37943506+teapfw@users.noreply.github.com>
2025-06-07 20:17:03 -0400

    Revert "try an alternate build type"
    This reverts commit db6d19d7691331ecba4236ae3364ee8d15a33b9c.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,34 @@
-const swconf = { cacheName: 'chirpy-1749341455',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/reflections/', '/robotics/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = {
+  
+    cacheName: 'chirpy-1749340397',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/reflections/',
+      
+        '/robotics/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
