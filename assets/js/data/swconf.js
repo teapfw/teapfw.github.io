86f3c51f99f7431dcf68859d416b0e7c8031b200
William Freidank <37943506+teapfw@users.noreply.github.com>
2025-07-06 10:33:44 -0400

    typo

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1751811402',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/reflections/', '/robotics/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1751812407',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/reflections/', '/robotics/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
