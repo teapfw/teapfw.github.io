142344a9f0967888b8ff33e702fb55d4577ead95
William Freidank <37943506+teapfw@users.noreply.github.com>
2025-10-21 21:52:36 -0400

    updates

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1761094030',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/reflections/', '/robotics/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1761097946',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/reflections/', '/robotics/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
